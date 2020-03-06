d52b138d0543c56d19d82b564b6661edb07abee1
KHWang <khwang@csie.fju.edu.tw>
2020-03-06 21:48:00 +0800

    Initial commit by khwang

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,6 +1,7 @@
 package com.mycompany.app;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -14,17 +15,16 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("Hi, FJCU Kuo-Hua Wang!");
         System.out.println("I am a teacher. You are students!");
+        System.out.println("Good Morning! Happy New Year!");
 
-        List<String> list = new ArrayList<String>();
+        List<String> list = new ArrayList<>();
         list.add("Boy"); list.add("Girl"); list.add("Old Man");
         for (String str: list) {
             System.out.println(str);
         }
-        Integer IntArray[] = {1, 2, 3, 4, 5};
-        ArrayList<Integer> list1 = new ArrayList<Integer>();
-        for (Integer i : IntArray) {
-            list1.add(i);
-        }
+        Integer[] IntArray = {1, 2, 3, 4, 5};
+        ArrayList<Integer> list1 = new ArrayList<>();
+        Collections.addAll(list1, IntArray);
         for (Integer a : list1) {
             System.out.println(a);
         }
