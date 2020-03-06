7ce02257760cc6ed24dcf0aac8111f3a2a8fcaab
KHWang <khwang@csie.fju.edu.tw>
2020-03-06 22:08:30 +0800

    Revert "Initial commit by khwang"
                                                                                        This reverts commit d52b138d

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,7 +1,6 @@
 package com.mycompany.app;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -15,16 +14,17 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("Hi, FJCU Kuo-Hua Wang!");
         System.out.println("I am a teacher. You are students!");
-        System.out.println("Good Morning! Happy New Year!");
 
-        List<String> list = new ArrayList<>();
+        List<String> list = new ArrayList<String>();
         list.add("Boy"); list.add("Girl"); list.add("Old Man");
         for (String str: list) {
             System.out.println(str);
         }
-        Integer[] IntArray = {1, 2, 3, 4, 5};
-        ArrayList<Integer> list1 = new ArrayList<>();
-        Collections.addAll(list1, IntArray);
+        Integer IntArray[] = {1, 2, 3, 4, 5};
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        for (Integer i : IntArray) {
+            list1.add(i);
+        }
         for (Integer a : list1) {
             System.out.println(a);
         }
